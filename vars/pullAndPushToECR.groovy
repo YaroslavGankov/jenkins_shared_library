8fc37c690b31b91362ea4026e3f9db3a0bb41a6c
YaroslavGankov <yaroslav.gankov@transportexchangegroup.com>
2023-02-13 13:58:00 +0200

    added spaces for formatting

--- vars/pullAndPushToECR.groovy
+++ vars/pullAndPushToECR.groovy
@@ -1,19 +1,26 @@
-def call(String source_image_repo, String destination_image_repo_hub, String destination_image_name) {
+def call(Map config = [:]) {
+  if (config.awsRegion == null) {
+    config.awsRegion="eu-west-1"
+  }
+  if (config.sourceImageHub == null) {
+      image_repo0="${config.sourceImageName}"
+  } else {
+      image_repo0="${config.sourceImageHub}/${config.sourceImageName}"
+  }
   sh """
-    echo "${source_image_repo}"
-    echo "${destination_image_repo_hub}"
-    echo "${destination_image_name}"
-    docker version
-    aws --version
-    aws sts get-caller-identity
-    aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin ${destination_image_repo_hub}
-    image_repo1="${destination_image_repo_hub}/${destination_image_name}";
-    tag=`echo ${source_image_repo} | cut -d : -f 2`
+    echo '\n     sourceImageHub:       ${config.sourceImageHub}\n \
+    sourceImageName:      ${config.sourceImageName}\n \
+    destinationImageHub:  ${config.destinationImageHub}\n \
+    destinationImageName: ${config.destinationImageName}\n \
+    awsRegion:            ${config.awsRegion}\n'
+    image_repo1="${config.destinationImageHub}/${config.destinationImageName}";
+    tag=`echo ${config.sourceImageName} | cut -d : -f 2`
     image_tag_latest="\${image_repo1}:latest"
     image_tag_current="\${image_repo1}:\${tag}"
-    echo "=============\nDEBUG:\nimage_tag_latest: \${image_tag_latest}\nimage_tag_current: \${image_tag_current}\n=============";
-    docker pull "${source_image_repo}"
-    docker tag "${source_image_repo}" "\${image_tag_latest}";
+    echo "\n     source:      ${image_repo0}\n     destination: \${image_tag_current}\n     destination: \${image_tag_latest}\n"
+    aws ecr get-login-password --region ${config.awsRegion} | docker login --username AWS --password-stdin ${config.destinationImageHub}
+    docker pull "${image_repo0}"
+    docker tag "${image_repo0}" "\${image_tag_latest}";
     docker tag "\${image_tag_latest}" "\${image_tag_current}";
     docker push "\${image_tag_latest}"
     docker push "\${image_tag_current}"
---
+++ vars/pullAndPushToECR_non_config.groovy
@@ -0,0 +1,21 @@
+def call(String source_image_repo, String destination_image_repo_hub, String destination_image_name) {
+  sh """
+    echo "${source_image_repo}"
+    echo "${destination_image_repo_hub}"
+    echo "${destination_image_name}"
+    docker version
+    aws --version
+    aws sts get-caller-identity
+    aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin ${destination_image_repo_hub}
+    image_repo1="${destination_image_repo_hub}/${destination_image_name}";
+    tag=`echo ${source_image_repo} | cut -d : -f 2`
+    image_tag_latest="\${image_repo1}:latest"
+    image_tag_current="\${image_repo1}:\${tag}"
+    echo "=============\nDEBUG:\nimage_tag_latest: \${image_tag_latest}\nimage_tag_current: \${image_tag_current}\n=============";
+    docker pull "${source_image_repo}"
+    docker tag "${source_image_repo}" "\${image_tag_latest}";
+    docker tag "\${image_tag_latest}" "\${image_tag_current}";
+    docker push "\${image_tag_latest}"
+    docker push "\${image_tag_current}"
+  """
+}
