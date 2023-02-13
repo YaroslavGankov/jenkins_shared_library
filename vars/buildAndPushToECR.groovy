13552336cef32761538609186b4dc7ae75641b3a
YaroslavGankov <yaroslav.gankov@transportexchangegroup.com>
2023-02-13 14:34:30 +0200

    used more groovy

--- vars/buildAndPushToECR.groovy
+++ vars/buildAndPushToECR.groovy
@@ -8,11 +8,7 @@ def call(Map config = [:]) {
       image_repo0="${config.sourceImageHub}/${config.sourceImageName}"
   }
   image_repo1_new="${config.destinationImageHub}/${config.destinationImageName}"
-  tag_new = config.sourceImageName.replaceAll(".*:","")
-  image_tag_latest_new="${image_repo1_new}:latest"
-  image_tag_current_new="${image_repo1_new}:${tag_new}"
-  println(image_tag_latest_new)
-  println(image_tag_current_new)
+  
   sh """
     echo '\n     sourceImageHub:       ${config.sourceImageHub}\n \
     sourceImageName:      ${config.sourceImageName}\n \
@@ -22,17 +18,19 @@ def call(Map config = [:]) {
     notPushTagLatest:     ${config.notPushTagLatest} (if null - push latest, if not null - not push latest tag to destination repo)'
     image_repo1="${config.destinationImageHub}/${config.destinationImageName}";
     tag=`echo ${config.sourceImageName} | cut -d : -f 2`
+    image_tag_latest="\${image_repo1}:latest"
+    image_tag_current="\${image_repo1}:\${tag}"
     echo "\n     source:      ${image_repo0}\n     destination: \${image_tag_current}\n     destination: \${image_tag_latest}\n"
     aws ecr get-login-password --region ${config.awsRegion} | docker login --username AWS --password-stdin ${config.destinationImageHub}
     docker pull "${image_repo0}"
-    docker tag "${image_repo0}" "${image_tag_latest_new}";
-    docker tag "${image_tag_latest_new}" "${image_tag_current_new}";
-    docker push "${image_tag_current_new}"
+    docker tag "${image_repo0}" "\${image_tag_latest}";
+    docker tag "\${image_tag_latest}" "\${image_tag_current}";
+    docker push "\${image_tag_current}"
   """
   if (config.notPushTagLatest == null) {
     sh """
-      echo "${image_tag_latest_new}"
-      docker push "${image_tag_latest_new}"
+      echo "\${image_tag_latest}"
+      docker push "\${image_tag_latest}"
     """
   }
 }
