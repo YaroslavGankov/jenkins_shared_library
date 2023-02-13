70826cd84d60ba5ac2b149bc69217881281ac369
YaroslavGankov <yaroslav.gankov@transportexchangegroup.com>
2023-02-13 15:04:57 +0200

    simple refactor

--- vars/pullAndPushToECR.groovy
+++ vars/pullAndPushToECR.groovy
@@ -1,10 +1,12 @@
 def call(Map config = [:]) {
+  //init
   if (config.awsRegion == null) {
     config.awsRegion="eu-west-1"
   }
   if (config.sourceImageHub == null) {
       image_repo0="${config.sourceImageName}"
-  } else {
+  }
+  else {
       image_repo0="${config.sourceImageHub}/${config.sourceImageName}"
   }
   image_repo1_new="${config.destinationImageHub}/${config.destinationImageName}"
@@ -12,9 +14,16 @@ def call(Map config = [:]) {
   image_tag_latest_new="${image_repo1_new}:latest"
   if (config.additionalTag == null) {
     image_tag_current_new="${image_repo1_new}:${tag_new}"
-  } else {
+  }
+  else {
     image_tag_current_new="${image_repo1_new}:${tag_new}-${config.additionalTag}"
   }
+  string_to_output="Pull-push image parameters:\n     source:      ${image_repo0}\n     destination: ${image_tag_current_new}\n"
+  if (config.notPushTagLatest == null) {
+    string_to_output="${string_to_output}     destination: ${image_tag_latest_new}\n"
+  }
+  
+  //info and debug
   echo "Input values:\n     sourceImageHub:       ${config.sourceImageHub}\n \
     sourceImageName:      ${config.sourceImageName}\n \
     destinationImageHub:  ${config.destinationImageHub}\n \
@@ -22,11 +31,9 @@ def call(Map config = [:]) {
     awsRegion:            ${config.awsRegion}\n \
     notPushTagLatest:     ${config.notPushTagLatest} (if null - push latest, if not null - not push latest tag to destination repo)\n \
     additionalTag:        ${config.additionalTag}"
-  string_to_output="Pull-push image parameters:\n     source:      ${image_repo0}\n     destination: ${image_tag_current_new}\n"
-  if (config.notPushTagLatest == null) {
-    string_to_output="${string_to_output}     destination: ${image_tag_latest_new}\n"
-  }
   echo "${string_to_output}"
+  
+  //pull-push
   sh """
     aws ecr get-login-password --region ${config.awsRegion} | docker login --username AWS --password-stdin ${config.destinationImageHub}
     docker pull "${image_repo0}"
