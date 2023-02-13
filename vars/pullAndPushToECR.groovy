a2d3c56fadf676d6c48c32498958e8636cfd5743
YaroslavGankov <yaroslav.gankov@transportexchangegroup.com>
2023-02-13 14:57:30 +0200

    used more groovy

--- vars/pullAndPushToECR.groovy
+++ vars/pullAndPushToECR.groovy
@@ -15,14 +15,18 @@ def call(Map config = [:]) {
   } else {
     image_tag_current_new="${image_repo1_new}:${tag_new}-${additionalTag}"
   }
-  echo "     sourceImageHub:       ${config.sourceImageHub}\n \
+  echo "Input values:\n     sourceImageHub:       ${config.sourceImageHub}\n \
     sourceImageName:      ${config.sourceImageName}\n \
     destinationImageHub:  ${config.destinationImageHub}\n \
     destinationImageName: ${config.destinationImageName}\n \
     awsRegion:            ${config.awsRegion}\n \
     notPushTagLatest:     ${config.notPushTagLatest} (if null - push latest, if not null - not push latest tag to destination repo)\n \
     additionalTag:        ${config.additionalTag}"
-  echo "\n     source:      ${image_repo0}\n     destination: ${image_tag_current_new}\n     destination: ${image_tag_latest_new}\n"
+  string_to_output="Pull-push image parameters:\n     source:      ${image_repo0}\n     destination: ${image_tag_current_new}\n"
+  if (config.notPushTagLatest == null) {
+    string_to_output="${string_to_output}     destination: ${image_tag_latest_new}\n"
+  }
+  echo "${string_to_output}"
   sh """
     aws ecr get-login-password --region ${config.awsRegion} | docker login --username AWS --password-stdin ${config.destinationImageHub}
     docker pull "${image_repo0}"
