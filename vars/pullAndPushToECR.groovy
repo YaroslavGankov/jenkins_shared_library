09900107bfabbf0be2ec6e8577a186d39ec7e268
YaroslavGankov <yaroslav.gankov@transportexchangegroup.com>
2023-02-13 11:57:46 +0200

    added pull-push operations for pullAndPushToECR file-function

--- vars/pullAndPushToECR.groovy
+++ vars/pullAndPushToECR.groovy
@@ -8,5 +8,20 @@ def call(String source_image_repo, String destination_image_repo_hub, String des
     aws --version
     aws sts get-caller-identity
     aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin ${destination_image_repo_hub}
+    image_name_to_pull="${source_image_repo}";
+    image_hub="${destination_image_repo_hub}";
+    image_name="${destination_image_repo}";
+    image_repo1="\${image_hub}/\${image_name}";
+    tag=`echo \$image_name_to_pull | cut -d : -f 2`
+    echo \$image_repo1;
+    image_tag_latest="\${image_repo1}:latest"
+    image_tag_current="\${image_repo1}:\${tag}"
+    echo "image_tag_latest: \${image_tag_latest}";
+    echo "image_tag_current: \${image_tag_current}";
+    docker pull "\${image_name_to_pull}"
+    docker tag "\${image_name_to_pull}" "\${image_tag_latest}";
+    docker tag "\${image_tag_latest}" "\${image_tag_current}";
+    docker push "\${image_tag_latest}"
+    docker push "\${image_tag_current}"
   """
 }
