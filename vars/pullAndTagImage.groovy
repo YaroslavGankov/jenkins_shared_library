c1cfe41b404594a7d00a04b0ea2ba198a5309d46
YaroslavGankov <yaroslav.gankov@transportexchangegroup.com>
2023-06-15 16:43:56 +0300

    Fix: fixed variables and added more debug

--- vars/pullAndTagImage.groovy
+++ vars/pullAndTagImage.groovy
@@ -1,7 +1,8 @@
+//pull image from private repo and tag it as image name is
 def call(Map config = [:]) {
   echo "DEBUG FUNCTION: config.dockerhub_private: ${config.dockerhub_private}"
   echo "DEBUG FUNCTION: config.image: ${config.image}"
   //tip - private_image = image
-  sh "docker pull ${dockerhub_private}/${image} && docker tag ${dockerhub_private}/${image} ${image};"
+  sh "docker pull ${config.dockerhub_private}/${config.image} && docker tag ${config.dockerhub_private}/${config.image} ${config.image};"
+  sh "docker images" //debug
 }
-
