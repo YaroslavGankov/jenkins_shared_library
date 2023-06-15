e5483247460afe8043d9a1159bd948bcb0562b4e
YaroslavGankov <yaroslav.gankov@transportexchangegroup.com>
2023-06-15 18:33:07 +0300

    fix: Function getTagBasedOnGithub. Added else if env.GIT_BRANCH isn't null

--- vars/getTagBasedOnGithub.groovy
+++ vars/getTagBasedOnGithub.groovy
@@ -3,7 +3,9 @@ def call() {
     DATE_PART =  sh(script: "git show -s --format=%ci | cut -d ' ' -f1 | tr -d -", returnStdout: true).trim() //format like 20230615
     println("DEBUG FROM FUNCTION getTagBasedOnGithub: env.GIT_BRANCH: "+env.GIT_BRANCH)//debug
     if ( env.GIT_BRANCH == null || env.GIT_BRANCH == '' ) {
-        GIT_BRANCH_IN_FUNCTION = sh(script: "git rev-parse --abbrev-ref HEAD | cut -d / -f 2- | tr '\\/' '_'", returnStdout: true).trim()
+        GIT_BRANCH_IN_FUNCTION = sh(script: "git rev-parse --abbrev-ref HEAD", returnStdout: true).trim()
+    } else {
+        GIT_BRANCH_IN_FUNCTION = env.GIT_BRANCH
     }
     // GIT_BRANCH_IN_FUNCTION = 'origin/main' //for debug
     start_position_for_cut=GIT_BRANCH_IN_FUNCTION.indexOf('/')
