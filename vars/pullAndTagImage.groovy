4a6f8cbc3a160a139f0aa0001de33897f4d394a8
YaroslavGankov <yaroslav.gankov@transportexchangegroup.com>
2023-06-15 16:50:24 +0300

    Test: Function pullAndTagImage. Commented debug steps

--- vars/pullAndTagImage.groovy
+++ vars/pullAndTagImage.groovy
@@ -1,8 +1,10 @@
-//pull image from private repo and tag it as image name is
+// // pull image from private repo and tag it as image name is
+// // example of usage with variables from params:
+// pullAndTagImage(dockerhub_private:params.ecr_repo,image:params.image_to_pull)
 def call(Map config = [:]) {
-  echo "DEBUG FUNCTION: config.dockerhub_private: ${config.dockerhub_private}"
-  echo "DEBUG FUNCTION: config.image: ${config.image}"
+  //echo "DEBUG FUNCTION: config.dockerhub_private: ${config.dockerhub_private}"
+  //echo "DEBUG FUNCTION: config.image: ${config.image}"
   //tip - private_image = image
   sh "docker pull ${config.dockerhub_private}/${config.image} && docker tag ${config.dockerhub_private}/${config.image} ${config.image};"
-  sh "docker images" //debug
+  //sh "docker images" //debug
 }
